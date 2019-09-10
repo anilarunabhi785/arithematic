4570e431b36370ca4a7c804dc38d35ba1c4c655a
Your Name <you@example.com>
2019-09-10 22:18:51 +0530

    simple intrest source code

---
+++ source/Simpleinterest.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Simpleinterest {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the principle");
+		long p=sc.nextLong();
+		System.out.println("enter the rate");
+		double r=sc.nextDouble();
+		System.out.println("enter the time");
+		int t= sc.nextInt();
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		double SI= (p*t*r)/100;
+		System.out.println(SI);}
+	}	
+	
+
+
