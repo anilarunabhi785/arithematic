267935ea8e928d82454f4f1c1b98c87b0481ec6b
Your Name <you@example.com>
2019-09-10 22:03:12 +0530

    source code for even odd numbers

---
+++ source/Evenodd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Evenodd {
+public static void main(String[] args) {
+	
+Scanner sc=new Scanner(System.in);
+		System.out.println("enter the values");
+		int a= sc.nextInt();
+	if(a%2==0) {
+		System.out.println("even number");}
+		else
+			System.out.println(" odd number");
+	}
+}
